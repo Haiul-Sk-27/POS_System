ea54f7fee592a581e33b64decbf3818425250638
Haiul SK <Haiulsk017@gmail.com>
2025-12-15 00:59:46 +0530

    POS_System

---
+++ StoreHub-pos-system/src/main/java/com/StoreHub/StoreHub/pos/system/StoreHubPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.StoreHub.StoreHub.pos.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreHubPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreHubPosSystemApplication.class, args);
+	}
+
+}
---
+++ StoreHub-pos-system/src/test/java/com/StoreHub/StoreHub/pos/system/StoreHubPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StoreHub.StoreHub.pos.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreHubPosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
