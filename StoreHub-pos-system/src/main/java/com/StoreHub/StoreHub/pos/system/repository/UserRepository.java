5e125e3a9ef7466e802cdf6a6d978177de25649e
Haiul SK <Haiulsk017@gmail.com>
2025-12-16 23:22:34 +0530

    StoreHub

---
+++ StoreHub-pos-system/src/main/java/com/StoreHub/StoreHub/pos/system/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.StoreHub.StoreHub.pos.system.repository;
+
+import com.StoreHub.StoreHub.pos.system.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
