08501d5aa6fcc96a2f28fbce30326554038aa75c
Haiul SK <Haiulsk017@gmail.com>
2026-04-13 20:32:25 +0530

    POS System Frontend

---
+++ StoreHub-pos-system/src/main/java/com/StoreHub/StoreHub/pos/system/utility/FileStorageUtil.java
@@ -0,0 +1,4 @@
+package com.StoreHub.StoreHub.pos.system.utility;
+
+public class FileStorageUtil {
+}
---
+++ pos_UI/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+  return (
+    <>
+    <h1 className="text-3xl font-bold bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent">
+      Tailwind test
+    </h1>
+    </>
+  )
+}
+
+export default App
